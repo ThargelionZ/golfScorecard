11849973105eb0fb3a72926e8bf9c0cdf4f558e8
ThargelionZ <measles10@gmail.com>
2016-11-04 10:30:26 -0600

    Made the table more dynamic

--- js/golfScorecard.js
+++ js/golfScorecard.js
@@ -14,6 +14,7 @@
 var myLocation = {latitude: 40.4426135, longitude: -111.8631116, radius: 100};
 var closeCourses;
 var selectedCourse;
+var numberOfHoles;
 
 $(document).ready(function() {
     $.post("http://golf-courses-api.herokuapp.com/courses/", myLocation, function(data, status){
@@ -29,39 +30,11 @@ function loadCourse(theid) {
         $(".teeOptions").remove();
         selectedCourse = JSON.parse(data);
         $("#courseTitle").html(selectedCourse.course.name);
+        numberOfHoles = selectedCourse.course.hole_count;
         for(var i = 0; i < selectedCourse.course.holes[0].tee_boxes.length - 1; i++){
             $("#teeTypes").append("<option class='teeOptions' value='" + i + "'>" + selectedCourse.course.holes[0].tee_boxes[i].tee_type +"</option>");
         }
     });
-}
-
-// var numberOfHoles = 18;
-//
-// function beginCard() {
-//     var players = $("#playerCount").val();
-//     $("#leftCard").html("");
-//     for(var i = 0, l = players; i < l; i++){
-//         $("#leftCard").append("<div id='playerLabel" + i + "' >Player " + (i + 1) + "<span class='glyphicon glyphicon-minus-sign' onclick='removePlayer("+ i +")'></span></div>");
-//     }
-//     for(var c = numberOfHoles; c >= 1; c--){
-//         $("#rightCard").append("<div id='column" + c + "'class='holcol'><div class='colheader'>" + c + "</div></div>");
-//     }
-//     $(".modalBackground").fadeOut();
-// }
-
-/*-------------------------------------------
- *   Create column titles
- -------------------------------------------*/
-
-for(var i = 0, h = 18; i < h; i++){
-        $("#holes").append("<td class='tableHeader'><strong>Hole " + (i + 1) + "</strong></td>");
-    if(i == 8){
-        $("#holes").append("<td class='tableHeader'><strong>Total (Front 9)</strong></td>");
-    }
-    else if(i == 17){
-        $("#holes").append("<td class='tableHeader'><strong>Total (Back 9)</strong></td>");
-        $("#holes").append("<td class='tableHeader'><strong>Grand Total</strong></td>");
-    }
 }
 
 /*-------------------------------------------
@@ -73,11 +46,28 @@ var playerAmount = 0;
 var itemNumber = 0;
 var leftCard;
 var errBool = false;
+var init = false;
 
 function addPlayer(){
+    if(!init){
+
+        // Create column titles
+
+        for(var i = 0; i < numberOfHoles; i++){
+            $("#holes").append("<td class='tableHeader'><strong>Hole " + (i + 1) + "</strong></td>");
+            if(i == 8){
+                $("#holes").append("<td class='tableHeader'><strong>Total (Out)</strong></td>");
+            }
+            else if(i == 17){
+                $("#holes").append("<td class='tableHeader'><strong>Total (In)</strong></td>");
+                $("#holes").append("<td class='tableHeader'><strong>Grand Total</strong></td>");
+            }
+        }
+        init = true;
+    }
     if(playerAmount > 5){
         if(!errBool){
-            $("#cardContainer").append("<p class='errorMessage' style='color: red;'>* Cannot add more than 6 players.</p>");
+            $("#cardContainer").append("<p id='errorMessage' style='color: red;'>* Cannot add more than 6 players.</p>");
             errBool = true;
         }
     } else {
@@ -96,18 +86,33 @@ function addPlayer(){
         // Create the rows associated with those players
 
         $("#input").append("<tr id='row" + playerNumber + "'></tr>");
-        for (var i = 0, h = 20; i <= h; i++) {
-            $("#row" + playerNumber).append("<td class='dataItem' id='dataItem" + itemNumber + "'>Test</td>");
-            itemNumber++;
+        if(numberOfHoles == 9){
+            for (var i = 0 ; i <= numberOfHoles; i++) {
+                $("#row" + playerNumber).append("<td class='dataItem' id='dataItem" + itemNumber + "'>" +
+                    "<input class='input' id='input" + i + "'>" +
+                    "</td>");
+                itemNumber++;
+            }
+        } else {
+            for (var i = 0 ; i <= numberOfHoles + 2; i++) {
+                $("#row" + playerNumber).append("<td class='dataItem' id='dataItem" + itemNumber + "'>" +
+                    "<input class='input' id='input" + i + "'>" +
+                    "</td>");
+                itemNumber++;
+            }
         }
         playerNumber++;
         playerAmount++;
     }
 }
 
-function changeName() {
-    $();
-}
+$(".player").on('click', function () {
+    console.log("this is working");
+    var playerName = $(this).val();
+    $(this).append("<input type='text' value='"+ playerName +"'>");
+    $(this).remove();
+});
+
 /*-------------------------------------------
  *   Create a way to remove players
  -------------------------------------------*/
@@ -115,7 +120,7 @@ function changeName() {
 
 function removePlayer(theid) {
     if(playerAmount <= 6){
-        $(".errorMessage").remove();
+        $("#errorMessage").remove();
         errBool = false;
     }
     $("#playerLabel" + theid).remove();
@@ -124,6 +129,8 @@ function removePlayer(theid) {
     if($("#leftCard").html() == ""){
         $("#leftCard").html("Click \"Add a Player\" to add players.");
         itemNumber = 0;
+        $(".tableHeader").remove();
+        init = false;
     }
 }
 
